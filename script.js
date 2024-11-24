56bc6e42841c6da3bccff0a40554f34ca9777201
Kyiem Chandler <chandlerkyiem4@gmail.com>
2024-11-24 17:03:44 -0500

    Added front end to structure recipe info

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 
-api_call = "https://api.spoonacular.com/recipes/random?apiKey=" + process.env.API_KEY + "&number=10";
+api_call = "https://api.spoonacular.com/recipes/random?apiKey=" + process.env.API_KEY + "&number=100";
 let apiResults = [];
 
 // Get API information for website
@@ -19,25 +19,66 @@ async function fetchData(url) {
     }
 }
 
-// Get the instructions, title, image
+
+var breakfastRecipes = {'breakfast':[]};
+var lunchRecipes = {'lunch':[]};
+let dinnerRecipes = {'dinner':[]};
+let sideRecipes = {'side':[]};
+let allRecipes = {'all':[]};
 
 (async () => {
     apiResults = await fetchData(api_call);
-    // console.log("All api results: ", apiResults)
 
-    // console.log(`Recipes ${apiResults.recipes}`)
+    // --Go through each recipe called by the api
     apiResults.recipes.forEach( item => {
-        console.log(`Title: ${item.title}\n\nImage: ${item.image}\n\nInstructions: ${item.instructions}`)
+
+        //  --check each occasion and add it to the appropriate json data var 
+        item.dishTypes.forEach(choice => {
+
+            // Get the instructions, title, image
+            data = {
+                title: item.title,
+                Image: item.image,
+                Ingredients: item.ingredients,     
+                Instructions: item.instructions
+                          
+            } 
+
+            // console.log("Recipe Info: ", data)
+            // --Push every recipe
+            allRecipes.all.push(data);
+
+            switch (choice) {
+                case 'breakfast':
+                    breakfastRecipes.breakfast.push(data)
+                    break;
+
+                case 'lunch':
+                    lunchRecipes.lunch.push(data)
+                    break;
+
+                case 'dinner':
+                    dinnerRecipes.dinner.push(data)
+                    break;
+
+                case ('snack' || 'side dish'):
+                    sideRecipes.side.push(data)
+                    break;
+
+                default:
+                    console.log("No meal Type received from API")
+                    break;
+            }       
+        })
     });
-
-    // viewResults = [
-    //     "Title" = apiResults[0].title,
-    //     "Image" = apiResults[0].image,
-    //     "Instructions" = apiResults[0].instructions
-    // ]
-
-    // console.log("Grab one with necessary info test: ", viewResults)
+    console.log("Breakfast recipes: ", breakfastRecipes.breakfast.length)
+    console.log("Lunch recipes: ", lunchRecipes.lunch.length)
+    console.log("Dinner recipes: ", dinnerRecipes.dinner.length)
+    console.log("Side dish Recipes: ", sideRecipes.side.length)
+    console.log("All recipes: ", allRecipes.all.length)
 })();
+
+
 
 
 
