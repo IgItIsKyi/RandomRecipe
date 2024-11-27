3be67ae73d77fb15dc26837bdc76879336bb7381
Kyiem Chandler <chandlerkyiem4@gmail.com>
2024-11-26 22:48:57 -0500

    converting project to express project pt 1

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+app.set("view engine", "ejs");
+const port = process.env.PORT || 3000;
+
+
+
+app.get('/', (req, res) => {
+    res.render("index.ejs")
+})
+
+
+app.use(express.static("public"))
+
+
+app.listen(port, () => {
+    console.log(`Listening on http://localhost:${port}`)
+}) 
